f6522c799d2b066b79c0a658cd0fef56bcddba89
liweiwh <liweiwh@itcast.cn>
2020-10-22 00:07:57 +0800

    频道编辑

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar" fixed>
-      <van-button class="search-btn" slot="title" type="info" size="small" round icon="search">搜索</van-button>
+    <van-nav-bar class="page-nav-bar"
+                 fixed>
+      <van-button class="search-btn"
+                  slot="title"
+                  type="info"
+                  size="small"
+                  round
+                  icon="search">搜索</van-button>
     </van-nav-bar>
     <!-- /导航栏 -->
 
@@ -12,41 +18,68 @@
       通过 animated 属性可以开启切换标签内容时的转场动画
       通过 swipeable 属性可以开启滑动切换标签页
      -->
-    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
-      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">
+    <van-tabs class="channel-tabs"
+              v-model="active"
+              animated
+              swipeable>
+      <van-tab :title="channel.name"
+               v-for="channel in channels"
+               :key="channel.id">
         <!-- 文章列表 -->
-        <article-list ref="article-list" :channel="channel" />
+        <article-list ref="article-list"
+                      :channel="channel" />
         <!-- 文章列表 -->
       </van-tab>
-      <div slot="nav-right" class="placeholder"></div>
-      <div slot="nav-right" class="hamburger-btn">
+      <div slot="nav-right"
+           class="placeholder"></div>
+      <div slot="nav-right"
+           class="hamburger-btn"
+           @click="isChennelEditShow = true">
         <i class="toutiao toutiao-gengduo"></i>
       </div>
     </van-tabs>
     <!-- /频道列表 -->
+
+    <!-- 频道编辑弹出层 -->
+    <van-popup v-model="isChennelEditShow"
+               closeable
+               close-icon-position="top-left"
+               position="bottom"
+               :style="{ height: '100%' }">
+      <channel-edit :my-channels="channels"
+                    :active="active"
+                    @update-active="onUpdateActive" />
+    </van-popup>
+    <!-- /频道编辑弹出层 -->
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
+import ChannelEdit from './components/channel-edit'
 
 export default {
   name: 'HomeIndex',
   components: {
-    ArticleList
+    ArticleList,
+    ChannelEdit
   },
   props: {},
   data () {
     return {
       active: 0,
-      channels: [] // 频道列表
+      channels: [], // 频道列表
+      isChennelEditShow: true // 控制编辑频道弹出层的显示状态
     }
   },
   computed: {},
   watch: {},
   created () {
     this.loadChannels()
+  },
+  mounted () {
+    window.home = this
   },
   methods: {
     async loadChannels () {
@@ -56,6 +89,14 @@ export default {
       } catch (err) {
         this.$toast('获取频道数据失败')
       }
+    },
+
+    onUpdateActive (index, isChennelEditShow = true) {
+      // 更新激活的频道项
+      this.active = index
+
+      // 关闭编辑频道弹层
+      this.isChennelEditShow = isChennelEditShow
     }
   }
 }
