1e0c3f79a5986e3078f236330fd4ee8515651f53
liweiwh <liweiwh@itcast.cn>
2020-10-22 00:30:06 +0800

    频道编辑保存

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -30,7 +30,13 @@
 </template>
 
 <script>
-import { getAllChannels } from '@/api/channel'
+import {
+  getAllChannels,
+  addUserChannel,
+  deleteUserChannel
+} from '@/api/channel'
+import { mapState } from 'vuex'
+import { setItem } from '@/utils/storage'
 
 export default {
   name: 'ChannelEdit',
@@ -53,6 +59,7 @@ export default {
     }
   },
   computed: {
+    ...mapState(['user']),
     // 计算属性会观测内部依赖数据的变化
     // 如果依赖的数据发生变化，则计算属性会重新执行
     recommendChannels () {
@@ -101,8 +108,24 @@ export default {
       }
     },
 
-    onAddChannel (channel) {
+    async onAddChannel (channel) {
       this.myChannels.push(channel)
+
+      // 数据持久化处理
+      if (this.user) {
+        try {
+          // 已登录，把数据请求接口放到线上
+          await addUserChannel({
+            id: channel.id, // 频道ID
+            seq: this.myChannels.length // 序号
+          })
+        } catch (err) {
+          this.$toast('保存失败，请稍后重试')
+        }
+      } else {
+        // 未登录，把数据存储到本地
+        setItem('TOUTIAO_CHANNELS', this.myChannels)
+      }
     },
 
     onMyChannelClick (channel, index) {
@@ -122,9 +145,26 @@ export default {
           // 让激活频道的索引 - 1
           this.$emit('update-active', this.active - 1, true)
         }
+
+        // 4. 处理持久化
+        this.deleteChannel(channel)
       } else {
         // 非编辑状态，执行切换频道
         this.$emit('update-active', index, false)
+      }
+    },
+
+    async deleteChannel (channel) {
+      try {
+        if (this.user) {
+          // 已登录，则将数据更新到线上
+          await deleteUserChannel(channel.id)
+        } else {
+          // 未登录，将数据更新到本地
+          setItem('TOUTIAO_CHANNELS', this.myChannels)
+        }
+      } catch (err) {
+        this.$toast('操作失败，请稍后重试')
       }
     }
   }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar"
-                 fixed>
-      <van-button class="search-btn"
-                  slot="title"
-                  type="info"
-                  size="small"
-                  round
-                  icon="search">搜索</van-button>
+    <van-nav-bar class="page-nav-bar" fixed>
+      <van-button class="search-btn" slot="title" type="info" size="small" round icon="search" to="/search">搜索</van-button>
     </van-nav-bar>
     <!-- /导航栏 -->
 
@@ -18,37 +12,22 @@
       通过 animated 属性可以开启切换标签内容时的转场动画
       通过 swipeable 属性可以开启滑动切换标签页
      -->
-    <van-tabs class="channel-tabs"
-              v-model="active"
-              animated
-              swipeable>
-      <van-tab :title="channel.name"
-               v-for="channel in channels"
-               :key="channel.id">
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
+      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">
         <!-- 文章列表 -->
-        <article-list ref="article-list"
-                      :channel="channel" />
+        <article-list ref="article-list" :channel="channel" />
         <!-- 文章列表 -->
       </van-tab>
-      <div slot="nav-right"
-           class="placeholder"></div>
-      <div slot="nav-right"
-           class="hamburger-btn"
-           @click="isChennelEditShow = true">
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn" @click="isChennelEditShow = true">
         <i class="toutiao toutiao-gengduo"></i>
       </div>
     </van-tabs>
     <!-- /频道列表 -->
 
     <!-- 频道编辑弹出层 -->
-    <van-popup v-model="isChennelEditShow"
-               closeable
-               close-icon-position="top-left"
-               position="bottom"
-               :style="{ height: '100%' }">
-      <channel-edit :my-channels="channels"
-                    :active="active"
-                    @update-active="onUpdateActive" />
+    <van-popup v-model="isChennelEditShow" closeable close-icon-position="top-left" position="bottom" :style="{ height: '100%' }">
+      <channel-edit :my-channels="channels" :active="active" @update-active="onUpdateActive" />
     </van-popup>
     <!-- /频道编辑弹出层 -->
   </div>
@@ -58,6 +37,8 @@
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
 import ChannelEdit from './components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 
 export default {
   name: 'HomeIndex',
@@ -70,22 +51,42 @@ export default {
     return {
       active: 0,
       channels: [], // 频道列表
-      isChennelEditShow: true // 控制编辑频道弹出层的显示状态
+      isChennelEditShow: false // 控制编辑频道弹出层的显示状态
     }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   created () {
     this.loadChannels()
   },
-  mounted () {
-    window.home = this
-  },
+  mounted () {},
   methods: {
     async loadChannels () {
       try {
-        const { data } = await getUserChannels()
-        this.channels = data.data.channels
+        // const { data } = await getUserChannels()
+        // this.channels = data.data.channels
+        let channels = []
+
+        if (this.user) {
+          // 已登录，请求获取用户频道列表
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        } else {
+          // 未登录，判断是否有本地的频道列表数据
+          const localChannels = getItem('TOUTIAO_CHANNELS')
+          //    有，拿来使用
+          if (localChannels) {
+            channels = localChannels
+          } else {
+            //    没有，请求获取默认频道列表
+            const { data } = await getUserChannels()
+            channels = data.data.channels
+          }
+        }
+
+        this.channels = channels
       } catch (err) {
         this.$toast('获取频道数据失败')
       }
